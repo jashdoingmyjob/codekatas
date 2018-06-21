db5f31d8a6ddb16c5b9f044ae518697b3b2ecef6
Jash Vora <jashvora@Jashs-MacBook-Pro.local>
2018-06-20 18:20:40 -0700

    final edits added new line case

--- CodeKatas/stringcalculator.py
+++ CodeKatas/stringcalculator.py
@@ -3,6 +3,7 @@ import re
 def add(num):
     if not num:
         return 0
+    num = num.replace('\n',',')
     print(num)
     num_list = [int(x) for x in num.split(',') if x.strip().isdigit()]
     sum =0
